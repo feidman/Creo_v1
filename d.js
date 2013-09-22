d4af3afb98930ef634d3f24d8dad7f091f7d3668
Dane Feagans <dane.feagans@gmail.com>
2013-09-21 20:02:20 -0400

    This is a push back from the Cloud9 IDE. The previous commit and push was from my AWS dev system and successfully pulled down to C9 with a simple git pull

--- d.js
+++ d.js
@@ -62,12 +62,12 @@ $('#OpenDrw').click(function(){
    //CREATES A DRW OF THE CURRENTLY OPENED PRT
    $('#ExportDrw').click(function(){
 
-    var server_handle = oSession.GetActiveServer(); //This works with the methods (result of method): .ActiveWorkspace (REPORT), .Location (http://pdm.prnet.us/Windchill),  .isActive (true), .Version (10.1), CollectWorkspaces().Count
+    var server_handle = oSession.GetActiveServer(); //This works with the methods (result of method) .ActiveWorkspace (REPORT), .Location (http://pdm.prnet.us/Windchill),  .isActive (true), .Version (10.1), CollectWorkspaces().Count
     var open_options = new ActiveXObject("pfc.pfcFileListOpt").FILE_LIST_LATEST;
     var dir = "wtws://" + server_handle.Alias + "/" + server_handle.ActiveWorkspace;
     document.getElementById("num_models").innerHTML = "Directory: " + dir;
     var files_seq = oSession.ListFiles("*.*",open_options,dir);
-    document.getElementById("part_select").innerHTML = "# of Parts: " + files_seq.Count;
+    //document.getElementById("part_select").innerHTML = "# of Parts: " + files_seq.Count;
     });
    
 });
