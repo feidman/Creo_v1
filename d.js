a80095be5a484e1a3fc815e587962c959457fcb5
dfeagans <dane.feagans@gmail.com>
2013-10-19 01:12:55 +0000

    Revert "This didn't work... and I forgot to test it. Move the weblink initialization stuff back to the top to make it global (so it can be used in function declrations too and not just at run-time). This is cleaner than the javascript window. syntax I originally used"
    This reverts commit fba8780d0b4db2017c8d6a5693fe7631546043a4.

--- d.js
+++ d.js
@@ -1,8 +1,3 @@
-//INITIALIZE CONNECTION TO PRO-E (mGlob and oSession are global for use in the DescFromPart function.
-var mGlob = new ActiveXObject("pfc.MpfcCOMGlobal"); //Makes connection to Pro-E
-var oSession = mGlob.GetProESession();  //Returns reference to current session to oSession
-
-//SHEET BEHAVIOR
 $(document).ready(function(){
     //Fades in menus
     $('div').hide().fadeIn(1000);
@@ -35,6 +30,10 @@ $(document).ready(function(){
 	}
 	$('#howto').slideToggle('fast');
     });
+
+//INITIALIZE CONNECTION TO PRO-E (The window. is javascript syntax to make mGlob and oSession global for use in the DescFromPart function since it's a function decleration its intepreted before this line is ran, so oSession would otherwise be out of scope of DescFromPart.
+window.mGlob = new ActiveXObject("pfc.MpfcCOMGlobal"); //Makes connection to Pro-E
+window.oSession = mGlob.GetProESession();  //Returns reference to current session to oSession
 
 //MAIN PROGRAM
 
